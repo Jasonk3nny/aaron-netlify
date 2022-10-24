388f51cd8720d3c69fd1f17b2da7446e293700c4
Jason Kenny <jkenny.mmt-b2021@fh-salzburg.ac.at>
2022-10-24 11:48:29 +0200

    added public folder with wordpress

---
+++ cms-2022-main-project/project/public/wp-content/themes/aaron/functions.php
@@ -0,0 +1,6 @@
+<?php
+function load_styles_and_scripts()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'load_styles_and_scripts');
---
+++ cms-2022-main-project/project/public/wp-content/themes/aaron/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Aaron</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <?php wp_body_open(); ?>
+    <h1>Hello Aaron</h1>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
